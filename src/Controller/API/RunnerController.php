381220e84bda44003fbb5ffe757db3e97402df9e
Marcos Teodoro <marcosvinici_us@live.com>
2021-03-12 18:52:38 -0300

    Implementa rota de cadastro de novos corredores

--- src/Controller/API/RunnerController.php
+++ src/Controller/API/RunnerController.php
@@ -5,6 +5,7 @@ namespace App\Controller\API;
 use App\Entity\Runner;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,7 +18,7 @@ class RunnerController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    #[Route('/api/runner', name: 'api/runner')]
+    #[Route('/api/runner', name: 'api/runner@index', methods:['GET'])]
     public function index(): Response
     {
         $entityRepository = $this->entityManager->getRepository(Runner::class);
@@ -28,6 +29,26 @@ class RunnerController extends AbstractController
             'status' => 'ok',
             'code' => 200,
             'runners' => $runners,
+        ]);
+    }
+
+    #[Route('/api/runner', name: 'api/runner@new', methods:['POST'])]
+    public function new(Request $request): Response
+    {
+        $requestData = $request->toArray();
+        $runner = new Runner();
+
+        $runner->setName($requestData['name']);
+        $runner->setCpf($requestData['cpf']);
+        $runner->setBirthdate((new \DateTime($requestData['birthdate'])));
+
+        $this->entityManager->persist($runner);
+        $this->entityManager->flush();
+
+        return $this->json([
+            'status' => 'ok',
+            'code' => 200,
+            'runner' => $this->container->get('serializer')->normalize($runner, null),
         ]);
     }
 }
--- tests/Controller/RunnerControllerTest.php
+++ tests/Controller/RunnerControllerTest.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RunnerControllerTest extends WebTestCase
 {
+    private $client;
+
     public function testIndexRunners()
     {
         $client = static::createClient();
@@ -18,5 +20,35 @@ class RunnerControllerTest extends WebTestCase
         $this->assertJson($response->getContent());
 
         $this->assertEquals(0, count(json_decode($response->getContent())->runners));
+    }
+
+    public function testNewRunner()
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/api/runner',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_REFERER' => '/foo/bar',
+            ],
+            json_encode([
+                'name' => 'John Doe',
+                'cpf' => '79396699071',
+                'birthdate' => '1994-07-03'
+            ])
+        );
+
+        $response = $client->getResponse();
+
+        $runnerData = json_decode($response->getContent())->runner;
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertNotEmpty($runnerData);
+
+        $this->assertIsNumeric($runnerData->id, "Runner ID needs to be numeric");
     }
 }
